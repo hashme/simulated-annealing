30fb15bc52462f5f1fe151542626bd57fc9121f8
Shariq Hashme <shariqhashme@gmail.com>
2015-02-18 18:16:36 -0500

    realized it was broken

--- annealing.py
+++ annealing.py
@@ -1,3 +1,5 @@
+import time
+import sys
 import itertools
 import math
 import random
@@ -62,26 +64,28 @@ representation_2 = l
 
 representation_3 = map(lambda x: set(range(20)) - x, representation_2)
 
-def neighbor(solution, max_distance = 1):
+def neighbor(selection, max_distance = 1):
  # distance : flipping pairs of bits
- new_solution = copy.deepcopy(solution)
+ new_selection = copy.deepcopy(selection)
  if max_distance > 100:
   max_distance = 100
  if max_distance < 0:
-  return new_solution
+  return new_selection
  so_far = 0
  so_far_small = set()
  while so_far < max_distance:
   patient_num = random.randint(0,19)
-  one_index = random.sample(representation_2[patient_num], 1)[0]
-  zero_index = random.sample(representation_3[patient_num], 1)[0]
+  patient_record = new_selection[patient_num]
+  not_patient_record = representation_2[patient_num] - patient_record
+  one_index = random.sample(patient_record, 1)[0]
+  zero_index = random.sample(not_patient_record, 1)[0]
   if (patient_num, one_index) not in so_far_small and (patient_num, zero_index) not in so_far_small:
    so_far_small.add((patient_num, one_index))
    so_far_small.add((patient_num, zero_index))
    so_far += 1
-   new_solution[patient_num][zero_index] = 1
-   new_solution[patient_num][one_index] = 0
- return new_solution
+   new_selection[patient_num].add(zero_index)
+   new_selection[patient_num].remove(one_index)
+ return new_selection
 
 def selection_solution(selection):
  solution = []
@@ -94,11 +98,21 @@ def selection_solution(selection):
 def solution_selection(solution):
  selection = []
  for k in range(20):
-  selection.append([])
+  selection.append(set())
   for j in range(21):
    if solution[k][j]:
-    selection[-1].append(j)
+    selection[-1].add(j)
  return selection
+
+def solution_distance(solution_1, solution_2):
+ return sum([sum([a!=b for a,b in zip(x,y)]) for x,y in zip(solution_1, solution_2)])
+
+distance_solution = solution_distance
+
+def selection_distance(selection_1, selection_2):
+ return solution_distance(selection_solution(selection_1), selection_solution(selection_2))
+
+distance_selection = selection_distance
 
 def score_selection(selection):
  all_pairs = set()
@@ -112,47 +126,65 @@ def score_solution(solution):
  selection = solution_selection(solution)
  return score_selection(selection)
 
-energy = score_solution
+energy = score_selection
 
 def random_selection():
  selection = []
  for level_number in range(20):
-  selection.append(random.sample(representation_2[level_number], 5))
+  selection.append(set(random.sample(representation_2[level_number], 5)))
  return selection
 
 def random_solution():
  return selection_solution(random_selection())
 
-def temperature(k, alpha = 0.8, initial_temp = 250):
- return (alpha ** k) * (float(initial_temp))
+def temperature_exp(alpha = 0.999, start = 300):
+ # T = (alpha ^ k) * start
+ def temp(k):
+  return (alpha ** k) * (float(start))
+ return temp
 
-def probability(energy_s, energy_n, T):
- if energy_n < energy_s:
+def temperature_lin(m = -0.01, start = 300):
+ # T = start + m*k
+ def temp(k):
+  return start + m * k
+ return temp
+
+def distance_f(temperature):
+ # returns a distance for neighbor, based on temperature
+ return 1
+
+def probability_f(energy_s, energy_n, T):
+ # probability that we switch
+ if energy_n >= energy_s:
   return 1
  if T <= 0.00000001:
   return T
- return math.exp(0-(energy_n - energy_s)/T)
+ return math.exp((energy_s - energy_n)/T)
 
-def distance(temperature, strangeness = False):
- # returns a distance for neighbor, based on temperature
- if strangeness:
-  # adjust this
-  return 210. * (temperature / 300.)
- else:
-  return 1
-
-def anneal(max_k = 1000, alpha = 0.99, initial_temp = 250, solution = None, strangeness = False):
- if solution is None:
-  solution = random_solution()
- max_score = energy(solution)
- best_solution = solution
+def anneal(max_k = 1000, temperature = temperature_exp(), probability = probability_f, distance = distance_f, selection = None):
+ last_time = 0
+ if selection is None:
+  selection = random_selection()
+ max_energy = energy(selection)
+ current_energy = max_energy
+ best_selection = selection
  for k in range(max_k):
-  T = temperature(k, alpha, initial_temp)
-  N = neighbor(solution, distance(T, strangeness))
-  score = energy(N)
-  if score > max_score:
-   max_score = score
-   best_solution = N
-  if probability(energy(solution), energy(N), T) > random.random():
-   solution = N
- return best_solution
+  T = temperature(k)
+  N = neighbor(selection, distance(T))
+  neighbor_energy = energy(N)
+  if time.time() > last_time + 0.25:
+   last_time = time.time()
+   sys.stdout.flush()
+   sys.stdout.write('Now: E={0},T={1} | Neighbor: E={2},D={3} | Best: E={4},D={5} | k={6}/{7}\r'.format(
+    str(current_energy).center(3, ' '), str(T)[:5].center(5, ' '), str(neighbor_energy).center(3, ' '),
+    str(distance_selection(N, selection)).center(3, ' '), str(max_energy).center(3, ' '), 
+    str(distance_selection(best_selection, selection)).center(3, ' '), str(k+1).center(len(str(max_k)), ' '), max_k))
+  if neighbor_energy > max_energy:
+   max_energy = neighbor_energy
+   best_selection = N
+  if probability(current_energy, neighbor_energy, T) >= random.random():
+   selection = N
+   current_energy = neighbor_energy
+ print '\b'
+ return best_selection
+
